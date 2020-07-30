633b65312be7201ad89fe1980a3dc579a2f3fda0
kevinkleinQC <kevin.klein@quantco.com>
2020-07-30 10:09:33 +0200

    Remove redundant file.

--- yaddo.py
+++
@@ -1,6 +0,0 @@
-x = "this is a super super long long long line this is really not cool asdf asdf asdf asdfa asdf asdfa it makes many things overflow"
-
-l = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-
-d = {'asdf':1,
-     'asdf':2}
