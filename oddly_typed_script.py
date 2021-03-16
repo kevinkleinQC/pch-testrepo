b043ac167509e7be71f1a4c67b4879d333e5eef1
kevinkleinQC <kevin.klein@quantco.com>
2021-03-16 13:25:14 +0100

    Provide environment and dotfiles.

--- oddly_typed_script.py
+++ oddly_typed_script.py
@@ -1,4 +1,8 @@
-from Typing import Optional
+# Note that when running this script, nothing seemingly out of the ordinary happens.
+# Yet, when feeding it to mypy...
+
+from typing import Optional, Union
+
 
 class Person:
     def __init__(self, name: str, height_in_cm: int, favorite_tea: str = None):
@@ -6,20 +10,27 @@ class Person:
         self.height_in_cm = height_in_cm
         self.favorite_tea = favorite_tea
 
+
 bob = Person("Bob", 172.5)
+
 
 def get_age(person: str) -> Optional[int]:
     if person == "Bob":
-      return 72
+        return 72
     if person == "Anne":
-      return 27
+        return 27
     return None
+
 
 print(get_age(bob))
 
-def pretty_print(simple_string: str):
-     print(f"*** {simple_string} ***")
 
-pretty_print(get_age(bob))
+def pretty_print(string_or_int: Union[str, int]):
+    print(f"*** {string_or_int} ***")
+
+
+print(get_age(bob.name) * 7)
+
+pretty_print(get_age(bob.name))
 
 pretty_print(bob.favorite_tea)
